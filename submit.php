c5d8d74e2d8dc2d7ea29ad9dc06c7121a2401e42
siriwan06 <siriwanpreesawing@gmail.com>
2023-11-21 11:06:03 +0700

    add submit.php

--- submit.php
+++ submit.php
@@ -4,10 +4,59 @@
     $city = $_POST['$city'];
     $web = $_POST['$web'];
     $role = $_POST['$role'];
-    $sign = $_POST['$sign'];
-    $sign1 = $_POST['$sign1'];
-    $sign2 = $_POST['$sign2'];
-    $sign3 = $_POST['3'];
+   
+
+    $servername = 'localhost : ';
+    $username = 'root';
+    $password = "";
+    $dbname = "db_employee";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("". $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO employee (username, password, city, webserver, role)
+            VALUES ($email,$password,$city,$web,$role)";
+
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+    } else {
+        echo"error" .$sql. "<br>" .$conn ->error;
+    }
+    $conn->close();
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     echo 'email: '.$email.'<br>';
     echo'password: '.$password.'<br>';
